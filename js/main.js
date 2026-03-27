3e3734cfb8d6e52bceb5418c4b80d5fc634d8ca2
Evgenii Borisovich <anarkilimitz@gmail.com>
2026-03-27 15:02:43 +0500

    Заменил картинку main и перекрасил кнопки

--- js/main.js
+++ js/main.js
@@ -7,31 +7,31 @@ initForm();
 initPhoneMask();
 
 // Для анимации
-ScrollReveal().reveal('section', {
-	delay: 0, // Задержка перед появлением секции
-	distance: '10px', // Расстояние, с которого элемент появляется
-	origin: 'bottom', // Направление появления
-	interval: 0, // Интервал между появлением элементов
-});
+// ScrollReveal().reveal('section', {
+// 	delay: 0, // Задержка перед появлением секции
+// 	distance: '10px', // Расстояние, с которого элемент появляется
+// 	origin: 'bottom', // Направление появления
+// 	interval: 0, // Интервал между появлением элементов
+// });
 
-ScrollReveal().reveal(
-	'.title, .promo__text, .present__text, .map__address, .scrolling-container, .btn',
-	{
-		delay: 200, // Задержка перед началом анимации
-		distance: '30px', // Расстояние, с которого элемент появляется
-		origin: 'bottom', // Направление появления (bottom, top, left, right)
-		duration: 1000, // Продолжительность анимации (в миллисекундах)
-		easing: 'ease-in-out',
-		interval: 200, // Интервал между появлением элементов
-		opacity: 0, // Начальная прозрачность
-		scale: 0.9, // Начальный масштаб (для эффекта "увеличения")
-		reset: false, // Анимация повторяется при каждой прокрутке
-	}
-);
+// ScrollReveal().reveal(
+// 	'.title, .promo__text, .present__text, .map__address, .scrolling-container, .btn',
+// 	{
+// 		delay: 200, // Задержка перед началом анимации
+// 		distance: '30px', // Расстояние, с которого элемент появляется
+// 		origin: 'bottom', // Направление появления (bottom, top, left, right)
+// 		duration: 1000, // Продолжительность анимации (в миллисекундах)
+// 		easing: 'ease-in-out',
+// 		interval: 200, // Интервал между появлением элементов
+// 		opacity: 0, // Начальная прозрачность
+// 		scale: 0.9, // Начальный масштаб (для эффекта "увеличения")
+// 		reset: false, // Анимация повторяется при каждой прокрутке
+// 	}
+// );
 // ------------ Video ----------------------
-const video = document.getElementById('promoVideo');
-// Устанавливаем скорость воспроизведения (0.5 = 50% скорости)
-video.playbackRate = 0.8;
+// const video = document.getElementById('promoVideo');
+// // Устанавливаем скорость воспроизведения (0.5 = 50% скорости)
+// video.playbackRate = 0.8;
 
 // Открытие/закрытие формы
 
