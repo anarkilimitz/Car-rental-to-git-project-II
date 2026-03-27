c8998940d4097497f610f79fcf372b54634f16ae
Evgenii Borisovich <anarkilimitz@gmail.com>
2026-03-27 18:51:41 +0500

    Добавил УСЛУГИ

--- js/main.js
+++ js/main.js
@@ -6,35 +6,6 @@ import { initPhoneMask } from './mask.js';
 initForm();
 initPhoneMask();
 
-// Для анимации
-// ScrollReveal().reveal('section', {
-// 	delay: 0, // Задержка перед появлением секции
-// 	distance: '10px', // Расстояние, с которого элемент появляется
-// 	origin: 'bottom', // Направление появления
-// 	interval: 0, // Интервал между появлением элементов
-// });
-
-// ScrollReveal().reveal(
-// 	'.title, .promo__text, .present__text, .map__address, .scrolling-container, .btn',
-// 	{
-// 		delay: 200, // Задержка перед началом анимации
-// 		distance: '30px', // Расстояние, с которого элемент появляется
-// 		origin: 'bottom', // Направление появления (bottom, top, left, right)
-// 		duration: 1000, // Продолжительность анимации (в миллисекундах)
-// 		easing: 'ease-in-out',
-// 		interval: 200, // Интервал между появлением элементов
-// 		opacity: 0, // Начальная прозрачность
-// 		scale: 0.9, // Начальный масштаб (для эффекта "увеличения")
-// 		reset: false, // Анимация повторяется при каждой прокрутке
-// 	}
-// );
-// ------------ Video ----------------------
-// const video = document.getElementById('promoVideo');
-// // Устанавливаем скорость воспроизведения (0.5 = 50% скорости)
-// video.playbackRate = 0.8;
-
-// Открытие/закрытие формы
-
 const openButtons = document.querySelectorAll('[data-action="open-form"]');
 const formModal = document.querySelector('.container-form');
 const closeBtn = document.querySelector('.close-form');
@@ -100,4 +71,38 @@ beforeSwiper.on('slideChange', () => {
 	setTimeout(() => {
 		afterSwiper.slideTo(beforeSwiper.activeIndex);
 	}, 150); // задержку второму слайдеру сделал для красоты
+});
+
+// ------------------- дата в футере----------------------
+// Автоматическое обновление года в футере
+document.getElementById('current-year').textContent = new Date().getFullYear();
+
+// -------  Услуги   -------
+
+const priceTriggers = document.querySelectorAll('[data-action="toggle-price"]');
+
+priceTriggers.forEach((trigger) => {
+	trigger.addEventListener('click', () => {
+		const parent = trigger.closest('.services__item');
+
+		// закрыть другие + обновить их кнопки
+		document.querySelectorAll('.services__item').forEach((item) => {
+			if (item !== parent) {
+				item.classList.remove('active');
+
+				const btn = item.querySelector('[data-action="toggle-price"]');
+				if (btn) btn.textContent = 'Подробнее / Прайс';
+			}
+		});
+
+		// переключение текущего
+		parent.classList.toggle('active');
+
+		// текст текущей кнопки
+		if (parent.classList.contains('active')) {
+			trigger.textContent = 'Скрыть прайс';
+		} else {
+			trigger.textContent = 'Подробнее / Прайс';
+		}
+	});
 });
